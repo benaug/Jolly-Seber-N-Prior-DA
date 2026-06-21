ed36bc4da70a750dfe73094049576966f30706a9
Ben Augustine <ba378@cornell.edu>
2026-06-21 15:55:01 -0400

    removed duplicate return

--- sim.JS.SCR.Dcov.mobileAC.R
+++ sim.JS.SCR.Dcov.mobileAC.R
@@ -129,6 +129,6 @@ sim.JS.SCR.Dcov.mobileAC <- function(D.beta0=NA,D.beta1=NA,D.cov=NA,InSS=NA,
   return(list(y=y,cov=cov,N=N,N.recruit=N.recruit,N.survive=N.survive,X=X,K=K,n.primary=n.primary,
               xlim=xlim,ylim=ylim,x.vals=x.vals,y.vals=y.vals,dSS=dSS,cells=cells,
               n.cells=n.cells,n.cells.x=n.cells.x,n.cells.y=n.cells.y,s.cell=s.cell,s=s,
-              D.cov=D.cov,InSS=InSS,res=res,cellArea=cellArea,N=N,lambda.y1=lambda.y1,
+              D.cov=D.cov,InSS=InSS,res=res,cellArea=cellArea,lambda.y1=lambda.y1,
               truth=truth))
 }
